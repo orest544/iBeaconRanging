f602f9adb760ef2ae807ec8d1b41d1671d85c1f0
Orest Patlyka <opatlyka@binariks.com>
2020-08-30 19:11:44 +0300

    Create project

---
+++ iBeaconRangingMonitoring/UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iBeaconRangingMonitoring
+//
+//  Created by Orest Patlyka on 30.08.2020.
+//  Copyright © 2020 Orest Patlyka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
